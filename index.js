ff968c1232ec3b1d12b634668e50ed435e9af209
Amit Kumar <amitmishra74880@gmail.com>
2022-12-28 00:06:13 +0530

    try

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const connect = require("./db/connect");
+const userRoute = require('./routes/user.route');
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 8080;
+
+app.use('/user',userRoute);
+
+app.get('/' , (req,res) =>{
+    res.send("server is working....");
+})
+
+app.get('/test' , (req,res)=>{
+    res.send("hello from test endpoint");
+})
+
+
+connect().then(()=>{
+    console.log("connected to database");
+}).catch((err) =>{
+    console.log("can't connect to database");
+})
+
+app.listen(port , () =>{
+    console.log(`server is running on port ${port}`);
+})
---
+++ routes/user.route.js
@@ -0,0 +1,28 @@
+const {Router} = require("express");
+const user = require('../models/user.model');
+
+const route = Router();
+
+
+
+route.post('/create' ,async (req,res)=>{
+    let temp = await user.create(req.body);
+    res.send("working");
+})
+
+route.get('/:email' , async(req,res)=>{
+    let {email} = req.params;
+    let temp = await user.find({email:{$eq:email}});
+    console.log(temp);
+    res.send("fin...");
+})
+
+
+
+
+
+
+
+
+
+module.exports = route;
