bd7350307954426bbca8bc2e79603dadcd758416
Amit Kumar <amitmishra74880@gmail.com>
2022-12-30 12:12:32 +0530

    server folder

---
+++ server/models/user.model.js
@@ -0,0 +1,26 @@
+
+const mongoose = require("mongoose");
+
+const userModel = new mongoose.Schema({
+
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+    
+
+})
+
+const user = mongoose.model('users' , userModel);
+
+module.exports = user;
