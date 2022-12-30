f3e0f8dec80253287d9b44013c83b921e1254c94
dhananjay <dhananjaykumarverma088@gmail.com>
2022-12-30 12:59:15 +0530

    product

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,6 @@
 require('dotenv').config()
 const mongoose = require("mongoose");
-
-
 async function connect(){
-
     return new Promise((resolve , reject) =>{
         mongoose.connect(process.env.DB , (err)=>{
             if(err){
